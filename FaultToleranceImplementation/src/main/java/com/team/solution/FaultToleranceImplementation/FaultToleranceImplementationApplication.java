af75a1982b9688da05410c004b2304d9c4c3b3f9
Dilane-Kamga <67242777+Dilane-Kamga@users.noreply.github.com>
2022-09-20 11:44:03 +0100

    Add files via upload

---
+++ FaultToleranceImplementation/src/main/java/com/team/solution/FaultToleranceImplementation/FaultToleranceImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.team.solution.FaultToleranceImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaultToleranceImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaultToleranceImplementationApplication.class, args);
+	}
+
+}
---
+++ FaultToleranceImplementation/src/test/java/com/team/solution/FaultToleranceImplementation/FaultToleranceImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.solution.FaultToleranceImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultToleranceImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
